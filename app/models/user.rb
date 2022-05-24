cc1ea71f22e21f9b9617e21cf515c859ffa46f73
Benjamin Semah <benjaminsemah@gmail.com>
2022-05-24 14:03:13 +0000

    Add login and registrations pages with device gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
+# rubocop:disable Layout/EndOfLine
+
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+  has_many :foods
+  has_many :recipes
 end
+# rubocop:enable Layout/EndOfLine
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,22 @@
 Rails.application.routes.draw do
-  resources :recipe_foods
-  resources :foods
-  resources :recipes
-  resources :users
+  devise_for :users
+
+  resources :users do
+    resources :foods
+    resources :recipes
+  end
+
+  resources :recipes do
+    resources :recipe_foods
+  end
+
+  resources :foods do
+    resources :recipe_foods
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "home#index"
 end
