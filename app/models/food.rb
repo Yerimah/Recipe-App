3d240a60a850578a68baa50084cec7e5ff1e06e2
Benjamin Semah <benjaminsemah@gmail.com>
2022-05-26 20:58:14 +0000

    Fix linter errors

--- app/models/food.rb
+++ app/models/food.rb
@@ -4,7 +4,7 @@ class Food < ApplicationRecord
   validates :name, presence: true
   validates :unit, presence: true
   validates :price, presence: true,
-                           numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+                    numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   has_many :recipe_foods, dependent: :destroy
 end
